4637ca2fa9bb8c65e4661527f1e73b1fc5c7f5ab
Topru <topru123@gmail.com>
2019-10-11 02:08:20 +0300

    player run and animation

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -1,7 +1,18 @@
 ﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public float speed = 6f;
+    public float walk_speed = 6f;
+    public float run_speed = 6f;
+
+    float Speed {
+        get {
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                return run_speed;
+            } else {
+                return walk_speed;
+            }
+        }
+    }
 
     Vector3 movement;
     Animator anim;
@@ -25,7 +36,7 @@ public class PlayerMovement : MonoBehaviour {
 
     void Move (float h, float v) {
         movement.Set(h, 0f, v);
-        movement = movement.normalized * speed * Time.deltaTime;
+        movement = movement.normalized * Speed * Time.deltaTime;
 
         rigidbody.MovePosition(transform.position + movement);
     }
@@ -45,8 +56,11 @@ public class PlayerMovement : MonoBehaviour {
     }
 
     void Animating (float h, float v) {
-        bool walking = (h != 0f || v != 0f);
-        anim.SetBool("Walk", walking);
+        bool walk = (h != 0f || v != 0f);
+        anim.SetBool("Walk", walk);
+        anim.SetBool("Run", Input.GetKey(KeyCode.LeftShift));
+
         anim.SetBool("SimpleAttack", Input.GetMouseButton(0));
+        
     }
 }
